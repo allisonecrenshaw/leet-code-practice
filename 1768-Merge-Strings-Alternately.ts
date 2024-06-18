9c1377f30c9f382e35e5102ebfa90d25a3a2aad9
Allison Crenshaw <allisonecrenshaw@gmail.com>
2024-06-18 14:11:24 -0400

    Repeat for practice.

--- 1768-Merge-Strings-Alternately.ts
+++ 1768-Merge-Strings-Alternately.ts
@@ -59,10 +59,42 @@ function mergeAlternately(word1: string, word2: string): string {
     return mergedWord;
 }
 
+function mergeAlternatelyRepeat1(word1: string, word2: string): string {
+    let word1Array: string[] = word1.split("");
+    let word2Array: string[] = word2.split("");
+
+    let mergedWord: string = "";
+    let i = 0;
+    while (i !== word1Array.length && i !== word2Array.length) {
+        mergedWord += word1Array[i];
+        mergedWord += word2Array[i];
+        i++;
+
+        if (
+            (i === word1Array.length && i !== word2Array.length) ||
+            (i === word2Array.length && i !== word1Array.length)
+        ) {
+            let remainingWordArray: string[] =
+                i === word1Array.length ? word2Array : word1Array;
+            while (i !== remainingWordArray.length) {
+                mergedWord += remainingWordArray[i];
+                i++;
+            }
+        }
+    }
+
+    return mergedWord;
+}
+
 function main() {
+    // Test1
     let word1 = "cat";
     let word2 = "hotdog";
-    let mergedWord = mergeAlternately(word1, word2);
+
+    // Test2
+    let word3 = "ac";
+    let word4 = "bdef";
+    let mergedWord = mergeAlternatelyRepeat1(word3, word4);
     console.log("Merged word: " + mergedWord);
 }
 
